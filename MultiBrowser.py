614c275369fc2b945c593fc374d90bcb324e177a
Anurag Sahu <Anurag.Sahu@vanderlande.com>
2020-03-04 17:29:22 +0530

    Initial Commit

---
+++ MultiBrowser.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+FILEDIR = "C:/Users/inasahu/PycharmProjects/SeleniumPython/"
+
+
+def select_browser(browser_name: str):
+    if browser_name.lower() == 'chrome':
+        driver = webdriver.Chrome(executable_path=FILEDIR + "Drivers/chromedriver.exe")
+        return driver
+    elif browser_name.lower() == 'firefox':
+        driver = webdriver.Firefox(executable_path=FILEDIR + "Drivers/geckodriver.exe")
+        return driver
+    elif browser_name.lower() == 'ie':
+        driver = webdriver.Ie(executable_path=FILEDIR + "Drivers/IEDriverServer.exe")
+        return driver
+    else:
+        print("Invalid Browser")
+
+
+driver = select_browser(input("Enter the browser: "))
+driver.get("http://www.newtours.demoaut.com/")
+driver.maximize_window()
+print(driver.title)
+print(driver.page_source)
+
+time.sleep(5)
+
+driver.close()
